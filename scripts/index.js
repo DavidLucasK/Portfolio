1f96ecb6aa85177b932eda634811011c08873e11
DavidLucasK <davidlucasfr70@gmail.com>
2024-02-15 16:38:22 -0300

    Companys

--- scripts/index.js
+++ scripts/index.js
@@ -60,7 +60,6 @@ function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split('');
   elemento.innerHTML = '';
   textoArray.forEach((letra, i) => {
-    console.log(i);
       setTimeout(function() {
         elemento.innerHTML += letra;
       }, 75 * i)
